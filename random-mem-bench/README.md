10c822efc4045d8bc9fa6da8b273a74f2b687a90
Marcin Zukowski <Marcin.Zukowski@snowflakecomputing.com>
2020-02-25 23:58:53 -0800

    Adding custom options

--- random-mem-bench/README.md
+++ random-mem-bench/README.md
@@ -1,3 +1,5 @@
+A simple random memory performance tester.
+
 ## Installation
 
 Setup Google Benchmark
@@ -17,3 +19,27 @@ Compile and run
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make -j 8
+./random-mem-bench
+```
+
+## Custom options
+
+Run `./random-mem-bench --help` to see all options, including both the custom ones
+and the Google Benchmark built-in ones. 
+
+Example custom options:
+* `range_log2_min` - log2 of the memory range minimum, default `4` (=> `16 bytes`)
+* `range_log2_min` - log2 of the memory range maximum, default `28` (=> `256MB`)
+* `range_log2_step` - log2 of the step when growing used memory, default `3` (=> step=`8`)
+* `threads_min` - min number of threads, default `1`
+* `threads_max` - max number of threads  , default `1`
+
+So, by default the benchmark will use a range of `16B` (`2**4`) to `256MB` (`2**28`), 
+with a step of `8` (`2**3`), on a single thread.
+
+To e.g. test memory ranges from 16 to 4GB, with up to 8 threads, run this
+```
+./random-mem-bench --range_log2_step=1 --range_log2_max=32 --threads_max=8
+```
+
+
--- random-mem-bench/main.cpp
+++ random-mem-bench/main.cpp
@@ -1,22 +1,42 @@
 #include "benchmark/benchmark.h"
 
 #include <cstdlib>
+#include <cstring>
 
 long rand64()
 {
   return (static_cast<int64_t>(rand()) << 32) | rand();
 }
 
+/// Number of items per round
 static constexpr int64_t PER_ROUND = 1'000'000;
 
+// Various options configurable via CLI
+static int64_t RANGE_LOG2_MIN = 4;
+static int64_t RANGE_LOG2_MAX = 28;
+static int64_t RANGE_LOG2_STEP = 3;
+static int64_t THREADS_MIN = 1;
+static int64_t THREADS_MAX = 1;
+
+// Are we in "--help" mode ?
+static bool IN_HELP = false;
+
+// Common setup
 class Bench : public ::benchmark::Fixture {
 public:
   void SetUp(const ::benchmark::State& st) override
   {
+    if (st.thread_index > 0) {
+      return;  // Only setup things in the 1st thread
+    }
     // Allocate memory
     int64_t range = st.range(0);
     m_mem = reinterpret_cast<int64_t*>(malloc(range));
     m_count = range / sizeof(*m_mem);
+    if (m_count <= 0) {
+      fprintf(stderr, "Range too small\n");
+      exit(1);
+    }
 
     // Fill with random integers in the range
     srand(0);
@@ -25,8 +45,11 @@ public:
     }
   }
 
-  void TearDown(const ::benchmark::State&) override
+  void TearDown(const ::benchmark::State& st) override
   {
+    if (st.thread_index > 0) {
+      return;   // Only tear down in the 1st thread
+    }
     free(m_mem);
   }
 
@@ -43,9 +66,8 @@ BENCHMARK_DEFINE_F(Bench, RandomWalk)(benchmark::State& state)
     }
     benchmark::DoNotOptimize(idx);
   }
-  state.SetItemsProcessed(state.iterations() * PER_ROUND);
+  state.SetItemsProcessed(state.iterations() * state.threads * PER_ROUND);
 }
-BENCHMARK_REGISTER_F(Bench, RandomWalk)->Range(1 << 4, 1 << 28);
 
 BENCHMARK_DEFINE_F(Bench, RandomSum)(benchmark::State& state)
 {
@@ -57,8 +79,119 @@ BENCHMARK_DEFINE_F(Bench, RandomSum)(benchmark::State& state)
     }
     benchmark::DoNotOptimize(sum);
   }
-  state.SetItemsProcessed(state.iterations() * PER_ROUND);
+  state.SetItemsProcessed(state.iterations() * state.threads * PER_ROUND);
 }
-BENCHMARK_REGISTER_F(Bench, RandomSum)->Range(1 << 4, 1 << 28);
 
-BENCHMARK_MAIN();
+/**
+ * Handle a single custom option.
+ * @return true if the particular option was detected and successfully parsed.
+ *         target is modified in this case
+ */
+bool parse_flag_int64(char *arg, const char *option, int64_t *target)
+{
+  size_t optLen = strlen(option);
+  if (strlen(arg) <= optLen + 3)
+    return false;  // too short
+  if (strncmp(arg, "--", 2) != 0)
+    return false;  // need "--"
+  arg += 2;
+  if (strncmp(arg, option, optLen) != 0)
+    return false;  // doesn't match
+  if (arg[optLen ] != '=')
+    return false;  // no '='
+  char *endPtr;
+  long long val = strtoll(arg + optLen + 1, &endPtr, 10);
+  if (*endPtr != '\0')
+    return false;  // not all parsed
+  *target = val;
+  return true;  // we're good
+}
+
+/// A single CLI option
+struct option
+{
+  const char *name;
+  int64_t *valPtr;
+};
+
+std::vector<option> options {
+    {"range_log2_min", &RANGE_LOG2_MIN},
+    {"range_log2_max", &RANGE_LOG2_MAX},
+    {"range_log2_step", &RANGE_LOG2_STEP},
+    {"threads_min", &THREADS_MIN},
+    {"threads_max", &THREADS_MAX},
+};
+
+/**
+ * Handle custom options. Returns unknown options, these will be passed to
+ * the default benchmark handler
+ */
+std::vector<char*> handle_custom_options(int argc, char** argv)
+{
+  std::vector<char*> remaining;
+  for (int i = 0; i < argc; i++) {
+    char* arg = argv[i];
+    bool found = false;
+    for (auto &o: options) {
+      if (parse_flag_int64(arg, o.name, o.valPtr)) {
+        found = true;
+        break;
+      }
+    }
+    if (strcmp(arg, "--help") == 0) {
+      fprintf(stderr, "\nCustom options:\n");
+      for (auto &o: options) {
+        fprintf(stderr, "    --%s=<int>\n", o.name);
+      }
+      fprintf(stderr, "\n");
+      IN_HELP = true;
+    }
+    if (!found) {
+      // We don't know this option, pass it on
+      remaining.push_back(arg);
+    }
+  }
+  return remaining;
+}
+
+/**
+ * Handle common options for all benchmarks
+ */
+void register_benchmarks()
+{
+  if (IN_HELP)
+    return;  // Don't register if in "--help"
+
+  fprintf(stderr, "Using custom settings:\n");
+  for (auto o: options) {
+    fprintf(stderr, "    %-15s : %lld\n", o.name, *o.valPtr);
+  }
+
+  // Known benchmarks
+  std::vector<benchmark::internal::Benchmark*> benchmarks{
+      new Bench_RandomSum_Benchmark(),
+      new Bench_RandomWalk_Benchmark(),
+  };
+
+  for (auto &b: benchmarks) {
+    b = ::benchmark::internal::RegisterBenchmarkInternal(b);
+    b = b->RangeMultiplier(1LL << RANGE_LOG2_STEP);
+    b = b->Range(1LL << RANGE_LOG2_MIN, 1LL << RANGE_LOG2_MAX);
+    if (THREADS_MAX > 1) {
+      b = b->ThreadRange(THREADS_MIN, THREADS_MAX);
+    }
+  }
+}
+
+int main(int argc, char** argv)
+{
+  std::vector<char*> remaining_options = handle_custom_options(argc, argv);
+  argc = remaining_options.size();
+  argv = remaining_options.data();
+
+  register_benchmarks();
+
+  benchmark::Initialize(&argc, argv);
+  if (benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  benchmark::RunSpecifiedBenchmarks();
+}
